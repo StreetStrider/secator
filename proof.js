7f58d73001ca9b4bf823be53d439ed373f5340c5
StreetStrider <teamfortresslife@gmail.com>
2014-12-08 18:37:35 +0300

    trying process on ast.

--- proof.js
+++ proof.js
@@ -8,4 +8,20 @@ var
 	name = process.argv[2] || 'test.js',
 	code = read(name, 'utf-8');
 
-console.log(code);
+var
+	ast = recast.parse(code);
+
+recast.visit(ast,
+{
+	visitIfStatement: function (path)
+	{
+		console.log(path);
+
+		path.value.alternate = null;
+		this.traverse(path);
+	},
+});
+
+code = recast.print(ast).code;
+
+process.stdout.write(code);
